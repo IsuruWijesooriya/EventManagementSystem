ffced581e9ad86373292f2a77c8c8a71b2e8ad88
Isuru Wijesooriya <isuruwijesooriya99@gmail.com>
2024-10-14 18:37:19 +0530

    Basic App

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Home from './pages/Home.jsx';
-import About from './pages/aboutus.jsx';
-import Admin from './pages/adminlogin.jsx';
-import Addevent from './pages/addevent.jsx';
-import Manageevent from './pages/manageevent.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import Home from './pages/home.jsx';
+import About from './pages/aboutus.jsx';
 
 function App() {
   return (
@@ -13,9 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/addevent" element={<Addevent />} />
-        <Route path="/manageevent" element={<Manageevent />} />
       </Routes>
     </Router>
   );
