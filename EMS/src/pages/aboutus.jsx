780de5efc1eb85384b5f34b82ef475143fbafefd
Isuru Wijesooriya <isuruwijesooriya99@gmail.com>
2024-10-20 16:54:34 +0530

    Edit

--- EMS/src/pages/aboutus.jsx
+++ EMS/src/pages/aboutus.jsx
@@ -26,7 +26,7 @@ const AboutUs = () => {
     {
       name: 'Isuru Wijesooriya',
       title: 'BSc(Hons) Eng. Computer Systems Engineering',
-      description: 'Developing effective marketing strategies for the digital era.',
+      description: 'Expert in full-stack development and project management.',
       image: '../src/assets/athur.png'
     }
   ];
