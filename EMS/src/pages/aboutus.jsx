dd22ae2b077e645bfb7f56af43b6aaba86ae33a5
Isuru Wijesooriya <isuruwijesooriya99@gmail.com>
2024-10-20 16:50:29 +0530

    Edit About

--- EMS/src/pages/aboutus.jsx
+++ EMS/src/pages/aboutus.jsx
@@ -9,25 +9,25 @@ const AboutUs = () => {
       name: 'Isuru Gunathilaka',
       title: 'BSc(Hons) Eng. Computer Systems Engineering',
       description: 'Expert in full-stack development and project management.',
-      image: './src/assets/isuru.jpg'
+      image: '../src/assets/isuru.jpg'
     },
     {
       name: 'Hirushika Nissanka',
       title: 'BSc(Hons) Eng. Computer Systems Engineering',
       description: 'Specialist in creating user-centered designs and experiences.',
-      image: './src/assets/hirushika.jpg'
+      image: '../src/assets/hirushika.jpg'
     },
     {
       name: 'Kasun Dhananjaya',
       title: 'BSc(Hons) Eng. Computer Systems Engineering',
       description: 'Focused on machine learning and data analysis.',
-      image: './src/assets/kasun.jpg'
+      image: '../src/assets/kasun.jpg'
     },
     {
       name: 'Isuru Wijesooriya',
       title: 'BSc(Hons) Eng. Computer Systems Engineering',
       description: 'Developing effective marketing strategies for the digital era.',
-      image: './src/assets/athur.png'
+      image: '../src/assets/athur.png'
     }
   ];
 
